f441d09b08c3c1869ac574ff6375c64d6306cc62
sofi <sofiamagaliramos@gmail.com>
2023-10-27 15:33:10 -0300

    initial

---
+++ sett.py
@@ -0,0 +1,29 @@
+token = 'bigdateros'
+
+whatsapp_token = 'tu token de wp'
+
+whatsapp_url = 'tu url de wp'
+whatsapp_phone_number_id = ' phone number id'
+openai_api_key= ' ingresa token openai'
+
+# 22-jul-2023 todos los tipos de media que soporta WhatsApp
+media_types = {
+    'audio/aac': 'aac',
+    'audio/mp4': 'mp4',
+    'audio/mpeg': 'mp3',
+    'audio/amr': 'amr',
+    'audio/ogg': 'ogg',
+    'text/plain': 'txt',
+    'application/pdf': 'pdf',
+    'application/vnd.ms-powerpoint': 'ppt',
+    'application/msword': 'doc',
+    'application/vnd.ms-excel': 'xls',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'docx',
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation': 'pptx',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': 'xlsx',
+    'image/jpeg': 'jpeg',
+    'image/png': 'png',
+    'video/mp4': 'mp4',
+    'video/3gp': '3gp',
+    'image/webp': 'webp',
+}
